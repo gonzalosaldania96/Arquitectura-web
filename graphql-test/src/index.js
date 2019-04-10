d6db28cfdd280ec51e313de58b4acc057a9964cf
Diego Marafetti <dmarafetti@gmail.com>
2019-04-10 17:24:11 -0300

    subscriptions

--- graphql-test/src/index.js
+++ graphql-test/src/index.js
@@ -1,7 +1,8 @@
-import { GraphQLServer } from 'graphql-yoga';
+import { GraphQLServer, PubSub } from 'graphql-yoga';
 import db from './db';
 import Query from './resolvers/query';
 import Mutation from './resolvers/mutation';
+import Subscription from './resolvers/subscription';
 import User from './resolvers/user';
 import Post from './resolvers/post';
 
@@ -10,10 +11,12 @@ import Post from './resolvers/post';
  * @since 1.0.0
  */
 
+const pubsub = new PubSub();
+
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
-    resolvers: {Query, Mutation, User, Post},
-    context: {db}
+    resolvers: {Query, Mutation, Subscription, User, Post},
+    context: {db, pubsub}
 });
 
 server.start(() => console.log('Server is running on localhost:4000'));
